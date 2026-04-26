3ebc62d61da649da029b52ae1915264375d1f6f0
Saku <saku@shiina.xyz>
2026-04-26 18:01:16 +0530

    ref

--- app/app.vue
+++ app/app.vue
@@ -1,42 +1,6 @@
-<script setup lang="ts">
-const colorMode = useColorMode()
-const { site } = useAppConfig()
-
-const themeColor = computed(() => colorMode.value === 'dark' ? '#09090B' : '#FFFBEB')
-
-useHead({
-  htmlAttrs: {
-    lang: 'en'
-  },
-  link: [
-    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-    { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
-    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
-    { rel: 'manifest', href: '/site.webmanifest' }
-  ],
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: themeColor }
-  ]
-})
-
-useSeoMeta({
-  title: site.title,
-  titleTemplate: `%s | ${site.name}`,
-  ogSiteName: site.name,
-  description: site.description,
-  ogDescription: site.description,
-  ogImage: '/og-image.png',
-  twitterCard: 'summary_large_image',
-  twitterImage: '/og-image.png'
-})
-</script>
-
 <template>
-  <UApp>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </UApp>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
 </template>
